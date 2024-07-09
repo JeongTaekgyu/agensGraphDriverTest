1928782002f2ff6f6912b47c8aba05a2fad1a94d
JeongTaekgyu <tk.jeong@bitnine.net>
2024-07-09 14:22:29 +0900

    agensgraph Driver 확인 테스트

---
+++ src/main/java/com/example/agensgraphdrivertest/model/TestMember.java
@@ -0,0 +1,20 @@
+package com.example.agensgraphdrivertest.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "table_testmember", schema = "public")
+public class TestMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+    private String password;
+    private String username;
+    private String email;
+}
---
+++ src/test/java/com/example/agensgraphdrivertest/AgensGraphDriverTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agensgraphdrivertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgensGraphDriverTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
